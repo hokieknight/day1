968dcb55af2255648187ac75f3fbdf5b88b04dac
hokieknight <dale.mrotek@gmail.com>
2023-12-08 23:14:50 -0500

    day2 stuff

--- hello.py
+++ hello.py
@@ -4,3 +4,9 @@ print("Hello " + name)
 length = len(name)
 print(length)
 print("hello world\nbye world")
+print("hello"[4])
+
+somenum = 123_456_789
+print(somenum)
+print(type(somenum))
+print(type(str(somenum)))
